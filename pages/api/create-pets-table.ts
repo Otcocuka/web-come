3d22614a9beded0854f759f1fa99b18c81d3c860
Otcocuka <d.ipharziev@gmail.com>
2023-09-05 20:37:22 +0300

    feat:added pnpm, prisma, postgres and created a docker image

--- pages/api/create-pets-table.ts
+++ pages/api/create-pets-table.ts
@@ -6,6 +6,16 @@ export default async function handler(
   response: NextApiResponse,
 ) {
   try {
+    const listTables = async () => sql`
+    SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+        AND table_type = 'BASE TABLE';
+`;
+
+const results = await listTables();
+
+console.log(results);
     const result =
       await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
     return response.status(200).json({ result });
