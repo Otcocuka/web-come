58c682d0bb59e57f48fb2710d30ab6fe843d9c61
Otcocuka <d.ipharziev@gmail.com>
2024-06-24 23:17:40 +0400

    abt section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
         "sm-lg": { min: "640px", max: "1024px" },
         "lg-xl": { min: "1024px", max: "1280px" },
       },
+      animation: {
+        'spin-slow': 'spin 60s linear infinite',
+      },
     },
   },
   plugins: [require("tw-elements/plugin.cjs")],
